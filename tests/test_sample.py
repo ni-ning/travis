a13e0a7e7d6f1de625600346917e2979c5a9bbed
nining <nining@gstianfu.com>
2019-11-22 16:44:39 +0800

    add test info

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -12,6 +12,6 @@ def test_answer():
 def test_environ():
     import os
     print(os.environ)
-    print(os.getenv('CORP_ID') is not None)
-    print(os.getenv('CORP_SECRET') is not None)
+    assert os.getenv('CORP_ID') is not None
+    assert os.getenv('CORP_SECRET') is not None
     assert True
